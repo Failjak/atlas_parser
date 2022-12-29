6fdce17e5828b3d823664d6df861fd09222fab86
Nikita Gushtyn <nikita.gushtyn200370@gmail.com>
2022-12-29 20:54:45 +0300

    git commit -m "replaced the parser with an api" (#8)

--- bot/handlers/base/handlers.py
+++ bot/handlers/base/handlers.py
@@ -7,14 +7,14 @@ from aiogram_datepicker import Datepicker
 from loguru import logger
 
 from bot.constants import DEFAULT_INTERVAL
-from bot.errors import InvalidUrlException, TicketsNotFoundException
 from bot.handlers.base.states import ChooseTripState
 from bot.handlers.base.utils import generate_final_route
 from bot.handlers.keyboard import get_markup
 from bot.settings import _get_datepicker_settings
-from parser.dto import ParserDto
-from parser.parser import run_parser
-from parser.settings import parser_settings
+from services.atlas.atlas_api import AtlasAPI
+from services.atlas.dto import DateTrips
+from services.atlas.exceptions import InvalidCity, TooFrequentRequests
+from services.parser.dto import ParserDto
 
 
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -60,8 +60,6 @@ async def _process_datepicker(callback_query: types.CallbackQuery, callback_data
 
     date = await datepicker.process(callback_query, callback_data)
     if date:
-        str_date = date.strftime('%Y-%m-%d')
-        await callback_query.message.answer(str_date)
         await state.update_data(date=date)
         await info_presentation(message=callback_query.message, state=state)
     else:
@@ -87,25 +85,27 @@ async def info_presentation(message: types.Message, state: FSMContext, **kwargs)
     await state.update_data(is_run=True)
     logger.log("BOT", f"Start parsing for chat_id: `{chat_id}`")
 
-    while (await dispatcher.storage.get_data(chat=chat_id)).get("is_run"):
-        try:
-            info = await run_parser(parser_settings, parser_dto)
+    api = AtlasAPI()
+    try:
+        dep_city = await api.get_departure_city_by_name(city_name=parser_dto.departure_place)
+        arrival_city = await api.get_arrival_city_by_name(from_id=dep_city.id, city_name=parser_dto.arrival_place)
 
-            if parser_dto.send_only_if_exist:
-                if info: await message.answer(info)
-            else:
-                await message.answer(info if info else "Нет мест")
+        while (await dispatcher.storage.get_data(chat=chat_id)).get("is_run"):
+            trips = await api.get_all_trips(dep_city, arrival_city, parser_dto.date)
+            logger.log("BOT", f"(chat_id: `{chat_id}`) Received a response from the API: {trips}")
 
             await asyncio.sleep(parser_dto.interval * 60)
 
-        except InvalidUrlException:
-            await message.answer("Рейсов не найдено. Измените точки маршрута.")
-            break
+            if parser_dto.send_only_if_exist:
+                if type(trips) == DateTrips:
+                    await message.answer(trips)
+            else:
+                await message.answer(trips)
 
-        except TicketsNotFoundException:
-            await message.answer(f"К сожалению, доступных рейсов на {parser_dto.date.strftime('%d %B')} нет")
-            break
+    except (InvalidCity, TooFrequentRequests) as e:
+        await message.answer(e.msg)
+    finally:
+        await state.update_data(is_run=False)
 
     logger.log("BOT", f"Parsing for chat_id: `{chat_id}` has been stopped")
     await state.finish()
-    await state.update_data(is_run=False)
---
+++ services/atlas/atlas_api.py
@@ -0,0 +1,89 @@
+import datetime
+from datetime import datetime
+from typing import List, Optional, Dict
+
+from services.atlas.api import API
+from services.atlas.constants import ATLAS_NAME_TO_ID
+from services.atlas.dto import City, DateTrips, Trip
+from services.atlas.exceptions import InvalidCity
+
+
+class AtlasAPI(API):
+    search_url = "https://atlasbus.by/api/search?from_id={}&to_id={}&calendar_width=30&date={}&passengers=1"
+    info_url = "https://atlasbus.by/api/search/suggest?user_input=&from_id={}&to_id={}&locale=ru"
+
+    def __init__(self):
+        pass
+
+    async def get_all_trips(self, departure_city: City, arrival_city: City, date: datetime.date) -> DateTrips | str:
+        url = self.search_url.format(departure_city.id, arrival_city.id, date.strftime("%Y-%m-%d"))
+        response = await self.send_arequest(url)
+
+        calendar = response.get("calendar")
+        rides = response.get("rides")
+
+        for day in calendar:
+            if day.get('date') != date.strftime("%Y-%m-%d"):
+                continue
+
+            if day.get("rideCount") == 0:
+                return "Нет билетов"
+            if not day.get("minPrices"):
+                return "Нет мест"
+
+            trips = self.get_rides_by_date(rides, date)
+            return DateTrips(date=date, trips=trips, trip_count=day.get("rideCount"))
+
+    @staticmethod
+    def get_rides_by_date(rides: List, date: datetime.date) -> List[Trip]:
+        trips = []
+        for ride in rides:
+            ride_date_str = ride.get('arrival')
+            ride_all_date = datetime.strptime(ride_date_str, '%Y-%m-%dT%H:%M:%S')
+            ride_date, ride_time = ride_all_date.date(), ride_all_date.time()
+
+            if ride_date == date:
+                trip = Trip(
+                    name=ride.get("name"),
+                    free_seats=ride.get("freeSeats"),
+                    departure_time=None,
+                    arrival_time=None,
+                    price=ride.get("price"),
+                    currency=ride.get("currency"),
+                )
+                trips.append(trip)
+
+        return trips
+
+    async def get_all_departure_cities(self) -> List[City]:
+        json_cities = await self.get_info_response(self.info_url)
+        cities = [City(**js_city) for js_city in json_cities]
+        return cities
+
+    async def get_all_arrival_cities(self, from_id: str) -> List[City]:
+        json_cities = await self.get_info_response(self.info_url, from_id=from_id)
+        cities = [City(**js_city) for js_city in json_cities]
+        return cities
+
+    async def get_all_departure_cities_as_dict(self) -> Dict:
+        json_cities = await self.get_info_response(self.info_url)
+        return {js_city.get('name'): City(**js_city) for js_city in json_cities}
+
+    async def get_all_arrival_cities_as_dict(self, from_id: str) -> Dict:
+        json_cities = await self.get_info_response(self.info_url, from_id=from_id)
+        return {js_city.get('name'): City(**js_city) for js_city in json_cities}
+
+    async def get_departure_city_by_name(self, city_name) -> Optional[City]:
+        dict_cities = await self.get_all_departure_cities_as_dict()
+        if city := dict_cities.get(city_name.capitalize()): return city
+        raise InvalidCity
+
+    async def get_arrival_city_by_name(self, from_id: str, city_name: str) -> Optional[City]:
+        dict_cities = await self.get_all_arrival_cities_as_dict(from_id=from_id)
+
+        if city := ATLAS_NAME_TO_ID.get(city_name.capitalize()): return city
+        if city := dict_cities.get(city_name.capitalize()): return city
+        raise InvalidCity
+
+    async def get_info_response(self, url, **kwargs):
+        return await self.send_arequest(url.format(kwargs.get("from_id", ""), kwargs.get("to_id", "")))
