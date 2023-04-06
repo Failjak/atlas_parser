5f9ff112c1854d1320275b30a5c7984e5c6318e7
Nikita Gushtyn <nikita.hu@datarails.com>
2023-04-06 19:18:33 +0300

    added example.env

--- bot/handlers/base/utils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-
-def generate_final_route(arrival_place, departure_place, date):
-    return f"{date.strftime('%Y-%m-%d') if isinstance(date, datetime.date) else date} " \
-           f"{departure_place.capitalize()} --> {arrival_place.capitalize()}"
